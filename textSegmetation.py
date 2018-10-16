f71bb3fb13d54b83f8f501ad3643b1bc56ed16ba
Vighnesh s <vighneshaka8921@gmail.com>
2018-10-16 14:59:03 +0530

    First step to the otsu binarization coded

--- textSegmetation.py
+++ textSegmetation.py
@@ -3,3 +3,10 @@ import numpy as np
 import os
 
 PATH_TO_FILE = "./text_data"
+
+img = cv.imread(os.listdir(PATH_TO_FILE)[0], 0)
+ret,img_otsu = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+cv.namedWindow('image', cv.WINDOW_NORMAL)
+cv.imshow('image',img_otsu)
+cv.waitKey(0)
+cv.destroyAllWindows()
