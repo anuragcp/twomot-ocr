dab510185356708f15a226f45ff3e6a90a7c407f
Vighnesh s <vighneshaka8921@gmail.com>
2018-10-16 15:34:53 +0530

    Made image dilation and erosion

--- textSegmetation.py
+++ textSegmetation.py
@@ -2,11 +2,15 @@ import cv2 as cv
 import numpy as np
 import os
 
-PATH_TO_FILE = "./text_data"
+#PATH_TO_FILE = "./text_data"
 
-img = cv.imread(os.listdir(PATH_TO_FILE)[0], 0)
-ret,img_otsu = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-cv.namedWindow('image', cv.WINDOW_NORMAL)
-cv.imshow('image',img_otsu)
+img = cv.imread("./text_data/ArialA.png", 0)
+print("Image loaded")
+ret, img_otsu = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+kernel = np.ones((5,5), np.uint8)
+img_erosion = cv.erode(img, kernel, iterations=1)
+img_dilation = cv.dilate(img, kernel, iterations=1)
+cv.namedWindow('image', cv.WINDOW_AUTOSIZE)
+cv.imshow('image', img_dilation)
 cv.waitKey(0)
 cv.destroyAllWindows()
